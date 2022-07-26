faed52c94cb3dd3b4ea042f32880b42037878ad5
PavelBabkin <pavel.babkin21@gmail.com>
2022-07-26 20:03:43 +0600

    2.2.1.spring_hibernate_version_2

--- src/main/java/hiber/service/UserService.java
+++ src/main/java/hiber/service/UserService.java
@@ -1,10 +1,16 @@
 package hiber.service;
 
+import hiber.model.Car;
 import hiber.model.User;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
+
+    void addCars(Car car);
+
     List<User> listUsers();
+
+    User userCar(String model, int series);
 }
